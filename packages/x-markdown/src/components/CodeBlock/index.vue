b82923db231c3c639b91470c1292ebde21e09644
菠萝吹雪 <58333667+yuguaa@users.noreply.github.com>
2025-12-03 17:25:02 +0800

    use shikijs/core

--- packages/x-markdown/src/components/CodeBlock/index.vue
+++ packages/x-markdown/src/components/CodeBlock/index.vue
@@ -73,8 +73,7 @@
 <script setup lang="ts">
 import { computed, ref, type CSSProperties } from 'vue'
 import type { BuiltinTheme, ThemedToken } from 'shiki'
-// import { getTokenStyleObject } from '@shikijs/core'
-import { getTokenStyleObject } from 'shiki'
+import { getTokenStyleObject } from '@shikijs/core'
 import { useClipboard } from '@vueuse/core'
 import { useHighlight } from '../../hooks/useHighlight'
 
