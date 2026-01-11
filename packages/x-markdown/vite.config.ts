866a0ae81909ab2b32cea9421bd323baa28c0f51
何嘉悦 <2834007710@qq.com>
2026-01-12 00:25:28 +0800

    fix: 动态加载、降级处理

--- packages/x-markdown/vite.config.ts
+++ packages/x-markdown/vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
-import { mermaidCheckPlugin } from './vite-plugin-mermaid-check'
-import { externalDynamicImportsPlugin } from './vite-plugin-external-dynamic-imports'
 
 export default defineConfig({
-  plugins: [vue(), mermaidCheckPlugin(), externalDynamicImportsPlugin()],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@components': resolve(__dirname, 'src'),
